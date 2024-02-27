5cd8ed4b0864f9a66ab47e0f05e0ca6becacecf5
Irina Moysevich <irina.moysevich@gmail.com>
2024-02-27 20:38:48 +0300

    updated exhibition pics order

--- exhibitions_cards.js
+++ exhibitions_cards.js
@@ -13,7 +13,7 @@ const exhibitions = [
         website: 'https://www.artspacemilano.com/portfolio/dreamars-iv/',
         name: 'DREAMERS IV',
         dates: '20.09. 2023 - 04.10.2023',
-        imgSrc: ['./images/exhibitions/dream.jpg', './images/exhibitions/dream1.jpg', './images/exhibitions/dream2.jpg'],
+        imgSrc: ['./images/exhibitions/dream.jpg', './images/exhibitions/dream1.jpg', './images/exhibitions/dream3.jpg'],
         descr: '"Irritation" was chosen for the exhibition "DREAMARS IV" in HUB/Art in Milan. The exhibition DREAMARS IV focused on topics such as the fight against discrimination both nationally and internationally, gender equality, and social inclusion. The participation in the exhibition of international artists will allow exchanging different visions on the issue. The title of the exhibition intends to "counter" violence, where the artists "dreamers" proposed the opposite version. The associations that deal with this issue will also participate in the presentation. ',
     },
     {
@@ -21,7 +21,7 @@ const exhibitions = [
         website: 'http://www.galleriacael.com',
         name: 'Orizzonti',
         dates: '10.02.2023 – 24.02.2023',
-        imgSrc: ['./images/exhibitions/oriz.jpg', './images/exhibitions/oriz2.jpg', './images/exhibitions/oriz1.jpg'],
+        imgSrc: ['./images/exhibitions/oriz.jpg', './images/exhibitions/oriz3.jpg', './images/exhibitions/oriz2.jpg'],
         descr: '"Anxious Joy" was chosen for the exhibition "Orizzonti" in Galleria Cael Milan. Horizon, in the visual sense, means the apparent line, circular in nature, that marks the boundaries of visibility from a place of observation and corresponds to the points where the sky appears to touch the earth. In a figurative sense, however, the horizon also means a field of action, scope of knowledge, or "that which is being prepared," referring to the near future. The horizon is also the limit of a quest or concept, that which contains everything within itself and delimits a certain meaning. The horizon always represents a limit, should one, in fact, move the point. ',
     },
     {
@@ -37,7 +37,7 @@ const exhibitions = [
         website: 'https://galeriaazur.art',
         name: 'HEARTBEAT',
         dates: '13.10.2022-07.11.2022',
-        imgSrc: ['./images/exhibitions/heart.jpg', './images/exhibitions/heart1.jpg', './images/exhibitions/heart2.jpg', './images/exhibitions/heart3.jpg', './images/exhibitions/heart4.jpg'],
+        imgSrc: ['./images/exhibitions/heart.jpg', './images/exhibitions/heart5.jpg', './images/exhibitions/heart2.jpg', './images/exhibitions/heart3.jpg', './images/exhibitions/heart4.jpg'],
         descr: 'Nika was represented at the "Heartbeat" exhibition at Galeria Azur Madrid with her works "Irritation" and "Hope." The greatness of the cultural imprint is strongly present in each of these artists; sweet, mestizo, and deep-rooted traditions merge with the vertigo of the speed our increasingly globalized planet is already turning. This show brings together a selection of works that invite you to pause and slowly observe the unity of breathing, the brushstrokes of their art, the vibrant and passionate pulse of their living blood. Like a walk through the secrets of incandescent creativity, where the viewer can discover the ups and downs of some fantastic and expressive works full of coherence, emotion, and reflection, the sublime curatorial journey uses stories, drawings, and collages of great compositions. ',
     },
     {
@@ -45,7 +45,7 @@ const exhibitions = [
         website: 'https://boomergallery.net',
         name: 'Vogue. Third Edition',
         dates: '10.2021',
-        imgSrc: ['./images/exhibitions/vogue.jpg', './images/exhibitions/vogue1.jpg', './images/exhibitions/vogue2.jpg', './images/exhibitions/vogue3.jpg'],
+        imgSrc: ['./images/exhibitions/vogue.jpg', './images/exhibitions/vogue2.jpg', './images/exhibitions/vogue3.jpg'],
         descr: '"Irritation" and "Anxious Joy" were presented by "Vogue Third Edition" in London. ',
     },
 
